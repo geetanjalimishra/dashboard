f0f65d0b64aa1ea1d577f47cc5b1e252f8e29d7b
Geetanjali Mishra <137869865+geetanjalimishra@users.noreply.github.com>
2024-02-04 18:15:02 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Login from "./Component/Login";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+import Routing from "./Component/Routing";
+
+function App() {
+  return (
+    <div>
+      <Routing />
+    </div>
+  );
+}
+
+export default App;
